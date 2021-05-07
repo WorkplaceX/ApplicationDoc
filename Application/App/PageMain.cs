8c6281e192bbbefdd406772a70ade7bc7d547150
WorkplaceX <info@workplacex.com>
2021-05-07 10:26:56 +0200

    Update page config grid

--- Application/App/PageMain.cs
+++ Application/App/PageMain.cs
@@ -20,7 +20,7 @@
             new Custom01(this);
 
             Container = new Div(this) { CssClass = "container" };
-            Content = new Div(Container) { CssClass = "content content-framework" };
+            Content = new Div(Container) { CssClass = "content content-bulma-framework" };
 
             GridNavigate = new GridNavigate(this) { IsHide = true };
             GridLanguage = new Grid<Language>(this) { IsHide = true };
